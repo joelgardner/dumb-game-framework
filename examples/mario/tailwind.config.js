fa1bcf11d8a817b5dc844877037502e71608ca62
Joel Gardner <williamjoelgardner@gmail.com>
2022-05-12 22:30:36 +0200

    Add fonts

---
+++ examples/mario/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "0rem",
+        lg: "4rem",
+        xl: "12rem",
+        "2xl": "18rem",
+      },
+      center: true,
+    },
+  },
+  plugins: [],
+};
